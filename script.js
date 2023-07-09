c2a5fb6927e27de8bc09ec02c23b4d50a0c308e6
rohan12114 <122142354+rohan12114@users.noreply.github.com>
2023-07-09 16:41:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function decimalToBinary(num) {
-  // Write your code here
+function decimalToBinary(decimal) {
+  let binary = "";
+  
+  while (decimal > 0) {
+    binary = (decimal % 2) + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  
+  return binary;
 }
 
-window.decimalToBinary = decimalToBinary;
+// Example usage:
+console.log(decimalToBinary(7)); // Output: 111
+console.log(decimalToBinary(10)); // Output: 1010
+console.log(decimalToBinary(33)); // Output: 100001
